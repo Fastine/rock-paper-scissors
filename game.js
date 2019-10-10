3775206726f31061d8cb8fa5253ed2e57efcf590
John Beaderstadt <jmbeaderstadt@gmail.com>
2019-10-09 23:59:58 -0500

    Added functionality to buttons, scoreboard, best 3 of 5, and the play screen.

--- game.js
+++ game.js
@@ -3,8 +3,17 @@
 "paper",
 "scissors"];
 
+//Scoreboard
     let cScore = 0;
     let pScore = 0;
+    let winner = document.createElement('p');
+    let playScreen = document.querySelector('#play');
+
+    function updateScore() {
+        document.getElementById("cScore").innerHTML = cScore;
+        document.getElementById("pScore").innerHTML = pScore;
+        declareWinner();
+    }
 
     // Computer randomized choice
     function computerPlay () {
@@ -13,47 +22,90 @@
 
     // One round of the game comparing choices between computer and player
     function playRound (playerSelection, computerSelection) {
-        var playerSelection = prompt("Rock, Paper, or Scissors?", "").toLowerCase();
+
+        // var playerSelection = "";
         var computerSelection = computerPlay();
 
         if (playerSelection == computerSelection) {
             pScore++;
             cScore++;
-            return ("Tie! 1 point each");
+            document.getElementById("play").innerHTML = "Tie! 1 point each";
+            updateScore();
         }
         else if (playerSelection == "rock")
             if (computerSelection == "scissors") {
                 pScore++;
-                return ("You win! Rock beats Scissors.") ; 
+                document.getElementById("play").innerHTML = "You win! Rock beats Scissors."; 
+                updateScore();
             }
             else {
                 cScore++;
-                return ("You lose! Paper beats Rock."); 
+                document.getElementById("play").innerHTML = "You lose! Paper beats Rock."; 
+                updateScore();
             }
         else if (playerSelection == 'paper')
             if (computerSelection == "rock") {
                 pScore++;
-                return ("You win! Paper beats Rock.");
+                document.getElementById("play").innerHTML = "You win! Paper beats Rock.";
+                updateScore();
             }
             else {
                 cScore++;
-                return ("You lose! Scissors beats Paper.");
+                document.getElementById("play").innerHTML = "You lose! Scissors beats Paper.";
+                updateScore();
             }
         else if (playerSelection == 'scissors')
             if (computerSelection == "paper") {
                 pScore++;
-                return ("You win! Scissors beats Paper.");
+                document.getElementById("play").innerHTML = "You win! Scissors beats Paper.";
+                updateScore();
             }
             else {
                 cScore++;
-                return ("You lose! Rock beats Scissors.");
+                document.getElementById("play").innerHTML = "You lose! Rock beats Scissors.";
+                updateScore();
+
             }
+    }
+
+    // Best 3 of 5
+
+    function declareWinner () {
+
+        if (pScore == 3 && cScore == 3) {
+            winner.textContent = "A tie!? Mulligan";
+            playScreen.appendChild(winner);
+            pScore = 0;
+            cScore = 0;
+        }
+        else if (pScore == 3 && cScore < 3) {
+            winner.textContent = "You won the match! Think you can win again?"
+            playScreen.appendChild(winner);
+            pScore = 0;
+            cScore = 0;
+        }
+        else if (cScore == 3 && pScore < 3) {
+            winner.textContent = "You have lost the match. You got the next one!"
+            playScreen.appendChild(winner);
+            pScore = 0;
+            cScore = 0;
+        }
     }
 
     // Buttons
     let rock = document.getElementById("rock");
     let paper = document.getElementById("paper");
     let scissors = document.getElementById("scissors");
+
+    rock.addEventListener("click", function(){playRound("rock")})
+    paper.addEventListener("click", function(){playRound("paper")})
+    scissors.addEventListener("click", function(){playRound("scissors")})
+
+    
+
+    // document.getElementById("rock").addEventListener("click", playRound("rock"))
+    // document.getElementById("paper").addEventListener("click", playRound("paper"))
+    // document.getElementById("scissors").addEventListener("click", playRound("scissors"))
 
 //     // 5 round game; scorekeeper
 //     function game () {
